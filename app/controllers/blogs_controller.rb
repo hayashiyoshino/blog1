b88861363e643c881bcc7cbae299968b0819bcfc
林佳志乃 <twinklestaryoshino@gmail.com>
2018-05-29 22:00:42 +0900

    indexからshowへリンク作る

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,15 +1,17 @@
 class BlogsController < ApplicationController
 
   def index
+    @blogs = Blog.all
+  end
+
+  def show
+    @blog = Blog.find(params[:id])
   end
 
   def new
   end
 
   def create
-  end
-
-  def show
   end
 
   def edit
