e538528fa740aca816f860d621f28b10391787df
林佳志乃 <twinklestaryoshino@gmail.com>
2018-05-30 19:12:13 +0900

    devise周り作成

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,9 @@
 class BlogsController < ApplicationController
 
+  before_action :move_to_index, except: [:index, :show]
+
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.order("id DESC")
   end
 
   def show
@@ -12,6 +14,8 @@ class BlogsController < ApplicationController
   end
 
   def create
+    Blog.create(create_params)
+    redirect_to action: :index
   end
 
   def edit
@@ -22,5 +26,15 @@ class BlogsController < ApplicationController
 
   def destroy
   end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_ingned_in?
+  end
+
+  def create_params
+    params.require(:blog).permit(:title, :text).merge(user_id: current_user.id)
+  end
+
 
 end
--- app/models/blog.rb
+++ app/models/blog.rb
@@ -1,2 +1,6 @@
 class Blog < ActiveRecord::Base
+
+  def round_created_at
+    self.created_at
+  end
 end
